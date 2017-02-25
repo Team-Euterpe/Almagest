3a25f0379a86dbc5dfb6729fc974d2602e8aafd4
SayakaIsBaka <jerome.tchan@epita.fr>
2017-02-25 19:13:35 +0100

    added basic cannon, need to work on collision with player

---
+++ Assets/Scripts/cannon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cannon : MonoBehaviour {
+
+	public GameObject barrel;
+	public GameObject projectile;
+	public float time = 0f;
+	public float force = 0f;
+	public float BPM = 0f;
+	private float rate;
+
+	void Start () 
+	{
+		rate = 60 / BPM;
+		InvokeRepeating ("Fire", 0f, rate);
+	}
+	void Fire()
+	{
+		GameObject temp_barrel = Instantiate (projectile, barrel.transform.position, projectile.transform.rotation);
+		Rigidbody temp_rigidbody = temp_barrel.GetComponent<Rigidbody> ();
+		temp_rigidbody.AddForce (transform.forward * force);
+		Destroy (temp_barrel, time);
+	}
+}
