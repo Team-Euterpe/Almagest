f37a4897a4cd163d504eca6946c3fad1c7b37c5e
SayakaIsBaka <jerome.tchan@epita.fr>
2017-02-26 15:01:32 +0100

    reorganized project, added bullet scipt

--- Assets/Scripts/cannon.cs
+++ Assets/Scripts/cannon.cs
@@ -6,18 +6,31 @@ public class cannon : MonoBehaviour {
 
 	public GameObject barrel;
 	public GameObject projectile;
+	public GameObject player;
+	private AudioSource fx;
 	public float time = 0f;
 	public float force = 0f;
 	public float BPM = 0f;
+	public float volume_ratio = 0;
 	private float rate;
 
 	void Start () 
 	{
 		rate = 60 / BPM;
+		fx = GetComponent<AudioSource>();
 		InvokeRepeating ("Fire", 0f, rate);
+	}
+	void Update ()
+	{
+		float vol = Vector3.Distance (player.transform.position, transform.position) / volume_ratio;
+		if (vol > 1.0)
+			fx.volume = 0;
+		else
+			fx.volume = 1 - vol;
 	}
 	void Fire()
 	{
+		fx.Play ();
 		GameObject temp_barrel = Instantiate (projectile, barrel.transform.position, projectile.transform.rotation);
 		Rigidbody temp_rigidbody = temp_barrel.GetComponent<Rigidbody> ();
 		temp_rigidbody.AddForce (transform.forward * force);
---
+++ Assets/Scripts/movie.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movie : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		MovieTexture movie = (MovieTexture)GetComponent<Renderer> ().material.mainTexture;
+		movie.loop = true;
+		movie.Play();
+	}
+}
