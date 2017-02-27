bb511f3d2886d16fa053ed8f8a2be3a599d4dc95
Titouan Le Falher <Titouan Le Falher>
2017-02-27 17:44:04 +0100

    added objectif

---
+++ Assets/Scripts/objectif.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectif : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            print("You win");
+            other.gameObject.SetActive(false);
+        }
+    }
+}
