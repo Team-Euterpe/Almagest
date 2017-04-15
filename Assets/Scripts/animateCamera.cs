2643cfb81e3747bb4fb1c394fceda54868b82c1f
SayakaIsBaka <jerome.tchan@epita.fr>
2017-04-15 17:01:32 +0200

    added main menu (wip)

---
+++ Assets/Scripts/animateCamera.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animateCamera : MonoBehaviour {
+
+	void Update () 
+	{
+		transform.Rotate (new Vector3 (0, -0.02f, 0.001f));
+	}
+}
---
+++ Assets/Scripts/menuManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class menuManager : MonoBehaviour 
+{
+	public void NewGame()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("crashtest");
+	}
+	public void ExitGame()
+	{
+		Application.Quit ();
+	}
+}
