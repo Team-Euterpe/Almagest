14ccc70054b0135892ec1726bc4752663b43706d
SayakaIsBaka <jerome.tchan@epita.fr>
2017-04-21 17:34:19 +0200

    player collisions added

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
 
-  float speed = 2.0f;
-  Vector2 pos;
-  Transform tr;
-  Rigidbody rb;
+  public float speed = 2.0f;
+  private Vector2 pos;
+  private Transform tr;
+  private Rigidbody rb;
+  private Vector3 previousact;
 
   // Use this for initialization
   void Start() {
@@ -18,19 +19,28 @@ public class player : MonoBehaviour
 
   // Update is called once per frame
   void Update() {
-    if (tr.position.x == pos.x && tr.position.z == pos.y) {
-      if (Input.GetKey("right")) {
-        pos += Vector2.right;
-      } else if (Input.GetKey("left")) {
-        pos += Vector2.left;
-      } else if (Input.GetKey("up")) {
-        pos += Vector2.up;
-      } else if (Input.GetKey("down")) {
-        pos += Vector2.down;
-      }
-    }
-    transform.position = Vector3.MoveTowards(transform.position, new Vector3(pos.x, transform.position.y, pos.y), Time.deltaTime * speed);
+		if (tr.position.x == pos.x && tr.position.z == pos.y) {
+			if (Input.GetKey ("right")) {
+				pos += Vector2.right;
+			} else if (Input.GetKey ("left")) {
+				pos += Vector2.left;
+			} else if (Input.GetKey ("up")) {
+				pos += Vector2.up;
+			} else if (Input.GetKey ("down")) {
+				pos += Vector2.down;
+			}
+			previousact = transform.position;
+		}
+		transform.position = Vector3.MoveTowards (transform.position, new Vector3 (pos.x, transform.position.y, pos.y), Time.deltaTime * speed);
 	if (transform.position.y < -20)
 		this.GetComponent<gameOver>().GameOver();
   }
+	void OnCollisionEnter(Collision other)
+	{
+		GetComponent<Rigidbody>().MovePosition(transform.position);
+		if (other.gameObject.CompareTag ("RealWall")) {
+			transform.position = previousact;
+			pos = new Vector2 (previousact.x, previousact.z); 
+		}
+	}
 }
