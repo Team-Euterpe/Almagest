acebceda169927b87ece2be6f2ab0f82550766f4
SKiEL <skielred@gmail.com>
2017-04-24 16:13:17 +0200

    objectif script calls deathCamera()

--- Assets/Scripts/objectif.cs
+++ Assets/Scripts/objectif.cs
@@ -3,23 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class objectif : MonoBehaviour {
+public class objectif : MonoBehaviour
+{
 
-	public Text winText;
-	public GameObject winParticle;
+  public Text winText;
+  public GameObject winParticle;
+  GameObject level;
+  void Start() {
+    winText.text = "";
+  }
 
-	void Start()
-	{
-		winText.text = "";
-	}
-
-	void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-			winText.text = "You win";
-			Instantiate (winParticle, this.transform.position, winParticle.transform.rotation);
-            other.gameObject.SetActive(false);
-        }
+  void OnTriggerEnter(Collider other) {
+    if (other.gameObject.CompareTag("Player")) {
+      winText.text = "You win";
+      Instantiate(winParticle, transform.position, winParticle.transform.rotation);
+      level = GameObject.FindGameObjectWithTag("Level");
+      level.GetComponent<gameLevel>().DeathCamera();
+      other.gameObject.SetActive(false);
     }
+  }
 }
