465fbf826e4460b04e0e053729d0bbc602ae4323
SKiEL <skielred@gmail.com>
2017-04-24 04:11:32 +0200

    Chronos. Early stuff. "Works" a bit.

--- Assets/Scripts/MovementCollision.cs
+++ Assets/Scripts/MovementCollision.cs
@@ -5,15 +5,13 @@ using UnityEngine;
 public class MovementCollision : MonoBehaviour {
 
   void OnTriggerEnter(Collider c) {
-    Debug.Log(c.tag);
     if (c.gameObject.CompareTag("Wall")) {
-      transform.parent.gameObject.GetComponent<player>().cannotmove.Add(this.name);
-      Debug.Log("a wall !");
+      transform.parent.gameObject.GetComponent<player>().cannotmove.Add(name);
     }
       
   }
   void OnTriggerExit(Collider c) {
     if (c.gameObject.CompareTag("Wall"))
-      transform.parent.gameObject.GetComponent<player>().cannotmove.Remove(this.name);
+      transform.parent.gameObject.GetComponent<player>().cannotmove.Remove(name);
   }
 }
--- Assets/Scripts/cheat.cs
+++ Assets/Scripts/cheat.cs
@@ -1,7 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using Chronos;
 
 public class cheat : MonoBehaviour {
 	
@@ -9,5 +8,11 @@ public class cheat : MonoBehaviour {
 	void Update () {
     if (Input.GetKeyDown(KeyCode.R))
       SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    if (Input.GetKeyDown(KeyCode.Space))
+      Timekeeper.instance.Clock("Root").localTimeScale = -1;
+    if (Input.GetKeyUp(KeyCode.Space)) {
+      Timekeeper.instance.Clock("Root").localTimeScale = 1;
+      //TODO reset timelines
+    }
 	}
 }
