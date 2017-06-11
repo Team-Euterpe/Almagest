201929fc06ede9429d8e35cb0ff7aa9a2fec5a77
SayakaIsBaka <jerome.tchan@epita.fr>
2017-06-11 17:47:35 +0200

    added button

---
+++ Assets/Scripts/button.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour {
+
+	public bool activated { get; private set; }
+	private AudioSource fx;
+
+	// Use this for initialization
+	void Start () {
+		activated = false;
+		fx = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void OnTriggerStay (Collider other) {
+		if (!other.CompareTag("Wall"))
+			activated = true;
+	}
+	void OnTriggerExit (Collider other) {
+		if (!other.CompareTag("Wall"))
+			activated = false;
+	}
+}
--- Assets/Scripts/lazer.cs
+++ Assets/Scripts/lazer.cs
@@ -14,6 +14,8 @@ public class lazer : MonoBehaviour {
 	private bool active;
 	public int range;
 	public Mode activation;
+	public GameObject button;
+	private button but;
 
 	// Use this for initialization
 	void Start () {
@@ -21,10 +23,12 @@ public class lazer : MonoBehaviour {
 		player = GameObject.FindGameObjectWithTag("Player");
 		bo = GetComponent<beatObject>();
 		active = false;
+		but = button.GetComponent<button> ();
 		ray = transform.GetChild (0).gameObject;
 		ray.transform.localPosition = new Vector3(0, 0, range);
 		ray.transform.localScale = new Vector3(ray.transform.localScale.x, range, ray.transform.localScale.z);
-		InvokeRepeating ("Fire", bo.offset, bo.rate);
+		if (activation != Mode.Button)
+			InvokeRepeating ("Fire", bo.offset, bo.rate);
 	}
 	
 	// Update is called once per frame
@@ -34,6 +38,17 @@ public class lazer : MonoBehaviour {
 			fx.volume = 0;
 		else
 			fx.volume = 1 - vol;
+		if (activation == Mode.Button) {
+			if (!active && !but.activated) {
+				fx.Play ();
+				active = true;
+				ray.SetActive (true);
+			} else if (active && but.activated) {
+				fx.Stop ();
+				active = false;
+				ray.SetActive (false);
+			}
+		}
 	}
 
 	void Fire()
