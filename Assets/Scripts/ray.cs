d36317568bc83f27cec56789612cbb226931b041
SayakaIsBaka <jerome.tchan@epita.fr>
2017-06-12 11:33:12 +0200

    laser now uses a raycast, fixed box

--- Assets/Scripts/ray.cs
+++ Assets/Scripts/ray.cs
@@ -4,7 +4,19 @@ using UnityEngine;
 
 public class ray : MonoBehaviour {
 
+
 	private float range;
+	private float orig_range;
+	private bool box;
+
+	void Start()
+	{
+		range = GetComponentInParent<lazer> ().range;
+		orig_range = range;
+		box = false;
+	}
+
+	/*
 	private GameObject box;
 	private bool isbox;
 
@@ -37,5 +49,36 @@ public class ray : MonoBehaviour {
 			transform.localPosition = new Vector3(0, 0, range);
 			transform.localScale = new Vector3(transform.localScale.x, range, transform.localScale.z);
 		}
+	}
+*/
+
+	void Update()
+	{
+		if (range > orig_range)
+			range = orig_range;
+		RaycastHit[] hits;
+		box = false;
+		hits = Physics.RaycastAll (transform.parent.position, transform.parent.forward, range);
+		foreach (RaycastHit hit in hits) {
+			if (hit.transform.gameObject.CompareTag ("Box")) {
+				box = true;
+				range = Mathf.Abs (Vector3.Distance (hit.transform.position, transform.parent.position));
+				ReziveOut ();
+			} else if (hit.transform.gameObject.CompareTag ("Player") && !box)
+				hit.transform.gameObject.GetComponent<gameOver> ().GameOver ();
+			if (box)
+				break;
+		}
+		if (!box) {
+			range = orig_range;
+			print (range);
+			ReziveOut ();
+		}
+	}
+
+	void ReziveOut()
+	{
+		transform.localPosition = new Vector3 (0, 0, range);
+		transform.localScale = new Vector3 (transform.localScale.x, range, transform.localScale.z);
 	}
 }
