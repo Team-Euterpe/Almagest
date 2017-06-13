dcb91144b587705743bf6449175cc0760e788df2
SayakaIsBaka <jerome.tchan@epita.fr>
2017-06-13 12:11:15 +0200

    added title in main menu

--- Assets/Scripts/menuManager.cs
+++ Assets/Scripts/menuManager.cs
@@ -2,9 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class menuManager : MonoBehaviour 
 {
+	public Image image;
+	public float fade;
+
+	void Start () {
+		image.GetComponent<CanvasRenderer> ().SetAlpha (0.0f);
+		image.CrossFadeAlpha (1f, fade, false);
+	}
+
 	public void NewGame()
 	{
 		UnityEngine.SceneManagement.SceneManager.LoadScene ("level0");
