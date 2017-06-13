a1b31b44aeb5e0aa786c1f50f8de80de8446e2d2
SayakaIsBaka <jerome.tchan@epita.fr>
2017-06-13 22:29:33 +0200

    added level select menu

--- Assets/Scripts/menuManager.cs
+++ Assets/Scripts/menuManager.cs
@@ -9,18 +9,41 @@ public class menuManager : MonoBehaviour
 {
 	public Image image;
 	public float fade;
+	private bool submenu;
+	public GameObject button1;
+	public GameObject button2;
+	public GameObject back;
 
 	void Start () {
 		image.GetComponent<CanvasRenderer> ().SetAlpha (0.0f);
 		image.CrossFadeAlpha (1f, fade, false);
+		submenu = false;
 	}
 
 	public void NewGame()
 	{
-		UnityEngine.SceneManagement.SceneManager.LoadScene ("level0");
+		if (!submenu) {
+			submenu = true;
+			button1.GetComponentInChildren<Text>().text = "Level 1";
+			button2.GetComponentInChildren<Text>().text = "Level 2";
+			back.SetActive(true);
+		}
+		else
+			UnityEngine.SceneManagement.SceneManager.LoadScene ("lvl");
 	}
 	public void ExitGame()
 	{
-		Application.Quit ();
+		if (!submenu)
+			Application.Quit ();
+		else
+			UnityEngine.SceneManagement.SceneManager.LoadScene ("level0");
+	}
+
+	public void BackMenu()
+	{
+		submenu = false;
+		button1.GetComponentInChildren<Text>().text = "Start Game";
+		button2.GetComponentInChildren<Text>().text = "Exit Game";
+		back.SetActive(false);
 	}
 }
