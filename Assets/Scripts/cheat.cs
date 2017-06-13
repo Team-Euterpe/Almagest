8ff6a2080e80538202ffe83cc0ab28bfc506bda3
SayakaIsBaka <jerome.tchan@epita.fr>
2017-06-13 17:26:07 +0200

    lvl0 update + added cheat

--- Assets/Scripts/cheat.cs
+++ Assets/Scripts/cheat.cs
@@ -21,8 +21,28 @@ public class cheat : MonoBehaviour
         player.GetComponent<player>().resetPos();
       }
     }
+		if (Input.GetKeyDown (KeyCode.Keypad1)) {
+			player.transform.position = new Vector3 (0, 1, 7);
+			player.GetComponent<player> ().resetPos ();
+		}
+		if (Input.GetKeyDown (KeyCode.Keypad2)) {
+			player.transform.position = new Vector3 (-5, 1, 26);
+			player.GetComponent<player> ().resetPos ();
+		}
+		if (Input.GetKeyDown (KeyCode.Keypad3)) {
+			player.transform.position = new Vector3 (-4, 4, 56);
+			player.GetComponent<player> ().resetPos ();
+		}
+		if (Input.GetKeyDown (KeyCode.Keypad4)) {
+			player.transform.position = new Vector3 (-3, 4, 98);
+			player.GetComponent<player> ().resetPos ();
+		}
 		if (Input.GetKeyDown (KeyCode.Keypad5)) {
 			player.transform.position = new Vector3 (-7, 1, 103);
+			player.GetComponent<player> ().resetPos ();
+		}
+		if (Input.GetKeyDown (KeyCode.Keypad6)) {
+			player.transform.position = new Vector3 (-17, 1, 114);
 			player.GetComponent<player> ().resetPos ();
 		}
   }
