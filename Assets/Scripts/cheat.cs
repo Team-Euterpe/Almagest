eb8102d99cdb76ffdc69f409fea60cf42f8b20ee
SKiEL <skielred@gmail.com>
2017-06-13 17:30:07 +0200

    Non-hardcoded inputs

--- Assets/Scripts/cheat.cs
+++ Assets/Scripts/cheat.cs
@@ -4,13 +4,13 @@ using Chronos;
 
 public class cheat : MonoBehaviour
 {
-	public GameObject player;
+  public GameObject player;
 
   // Update is called once per frame
   void Update() {
-    if (Input.GetKeyDown(KeyCode.R))
+    if (Input.GetButton("Fast retry"))
       SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    if (Input.GetKeyDown(KeyCode.Space)) {
+    if (Input.GetButton("Rewind")) {
       Timekeeper.instance.Clock("Root").localTimeScale = -1;
       this.gameObject.GetComponent<AudioSource>().pitch = -1;
     }
@@ -21,9 +21,29 @@ public class cheat : MonoBehaviour
         player.GetComponent<player>().resetPos();
       }
     }
-		if (Input.GetKeyDown (KeyCode.Keypad5)) {
-			player.transform.position = new Vector3 (-7, 1, 103);
-			player.GetComponent<player> ().resetPos ();
-		}
+    if (Input.GetKeyDown(KeyCode.Keypad1)) {
+      player.transform.position = new Vector3(0, 1, 7);
+      player.GetComponent<player>().resetPos();
+    }
+    if (Input.GetKeyDown(KeyCode.Keypad2)) {
+      player.transform.position = new Vector3(-5, 1, 26);
+      player.GetComponent<player>().resetPos();
+    }
+    if (Input.GetKeyDown(KeyCode.Keypad3)) {
+      player.transform.position = new Vector3(-4, 4, 56);
+      player.GetComponent<player>().resetPos();
+    }
+    if (Input.GetKeyDown(KeyCode.Keypad4)) {
+      player.transform.position = new Vector3(-3, 4, 98);
+      player.GetComponent<player>().resetPos();
+    }
+    if (Input.GetKeyDown(KeyCode.Keypad5)) {
+      player.transform.position = new Vector3(-7, 1, 103);
+      player.GetComponent<player>().resetPos();
+    }
+    if (Input.GetKeyDown(KeyCode.Keypad6)) {
+      player.transform.position = new Vector3(-17, 1, 114);
+      player.GetComponent<player>().resetPos();
+    }
   }
 }
